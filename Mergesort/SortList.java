8ddf1e44cb0e5af9a4c2523c41e83247e097ac0b
chiranth <chiranthlp23@gmail.com>
2025-07-01 15:21:08 +0530

    Fix merge logic in SortList to correctly link nodes during merging

--- Mergesort/SortList.java
+++ Mergesort/SortList.java
@@ -38,11 +38,11 @@ public class SortList {
 
         while(l1 != null && l2 != null){
             if(l1.val < l2.val){
-                tail = l1;
+                tail.next = l1;
                 l1 = l1.next;
             }
             else{
-                tail = l2;
+                tail.next = l2;
                 l2 = l2.next;
             }
 
